9156622651ce810392013b568c1e3d348e382b9f
migue <miguelalonso283@gmail.com>
2026-05-19 20:35:26 +0200

    Para saber cómo funcionan las clases abstractas. Tenemos la clase bastracta "Figura", de la cual heredan las clases "Circulo" y "Rectangulo", obligándolas a tener el método "calcularArea()" y que puedan "imprimirInfo()" sin necesidad de escribir el método en cada clase.

---
+++ src/CalsesAbstractas/Circulo.java
@@ -0,0 +1,15 @@
+package CalsesAbstractas;
+
+public class Circulo extends Figura {
+
+    double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    double calcularArea(){
+        return Math.PI * Math.pow(radio,2);
+    }
+}
