d726060081e13fc2cfc7f1accb4316be84d9df69
Miguel Alonso <miguelalonso283@gmail.com>
2026-08-16 17:48:12 +0200

    Varios ejemplos javascript

---
+++ practice.js
@@ -0,0 +1,21 @@
+// for (let i=10;i<=100;i+=10) {
+//     console.log(i)
+// }
+
+// let messages = [
+//     "Hola bro",
+//     "Hola, ¿Cómo estas?",
+//     "Estoy bien",
+//     "Genial"
+// ]
+
+// for (let i = 0; i < messages.length; i++) {
+//     console.log(messages[i])
+// }
+
+let sentence = ["Hello","my","name","is","Miguel"]
+let greetings = document.getElementById("greetings-el")
+
+for (let i = 0; i < sentence.length; i++) {
+    greetings.textContent += sentence[i] + " "
+}
