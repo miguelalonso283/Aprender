7cdc2150ce0ca4c98f2e891b4bc9b6f1686f346e
Miguel Alonso <miguelalonso283@gmail.com>
2026-08-16 20:27:59 +0200

    Ejemplo con el operador switch

--- practice.js
+++ practice.js
@@ -32,8 +32,35 @@
 // const message = exerciseTimeMins < 30 ? 'You need to try harder' : exerciseTimeMins < 60 ? 'Doing good!' : 'Excellent!'
 // console.log(message)
 
-const playerGuess = 3
-const correctAnswer = 6
+// const playerGuess = 3
+// const correctAnswer = 6
 
-const message = playerGuess === correctAnswer ? 'Correct!' : playerGuess < correctAnswer ? 'Higher' : 'Lower'
-console.log(message)
+// const message = playerGuess === correctAnswer ? 'Correct!' : playerGuess < correctAnswer ? 'Higher' : 'Lower'
+// console.log(message)
+
+/*
+Precios:
+Cafe $2
+Sandwiches $5
+Ensalada $4
+Tarta de limon $3
+*/
+
+function selectItem(item) {
+
+    let price = 0
+
+    switch(item) {
+
+        case 'Cafe':
+            price = 2
+        case 'Sandwich':
+            price = 5
+        case 'Ensalada':
+            price = 4
+        break
+    }
+    return `Has seleccionado ${item}. Serían $${price} porfavor`
+}
+
+console.log(selectItem('Sandwich'))
